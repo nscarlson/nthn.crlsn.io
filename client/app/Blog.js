24b578f91ef603f20a51281f0fc9a2a6666e08e1
Nathan Carlson <nathan.s.carlson@gmail.com>
2016-03-27 22:22:03 -0600

    README added

---
+++ client/app/Blog.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+//import routes from './config/BlogRoutes';
+
+ReactDOM.render(
+  //<Router>{routes}</Router>,
+  document.getElementById('nthncrlsn')
+)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: "./app/App.js",
+  entry: "./app/Blog.js",
   output: {
     filename: "public/bundle.js"
   },
