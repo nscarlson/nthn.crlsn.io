866897fd59c1c99f2acc72b98485dd1fda349432
Nathan Carlson <nathan.s.carlson@gmail.com>
2016-04-14 20:12:16 -0600

    Frontend development stack ready to roll!
    Webpack sass loader
    React
    Neat/Bourbon
    
    Basic navbar

--- client/app/main.jsx
+++ client/app/main.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {ArticleList} from 'app/components/blog/ArticleList';
+import { Navbar }     from 'app/components/Navbar/Navbar';
+import { ArticleList }from 'app/components/ArticleList';
+import { Footer }     from 'app/components/Footer/Footer.jsx';
 
-ReactDOM.render(<ArticleList/>, document.getElementById('app') || document.body);
+ReactDOM.render(
+  <div id="wrapper">
+    <Navbar/>
+    <ArticleList/>
+  </div>,
+  document.getElementById('test') || document.body
+);
+
+//ReactDOM.render(<Footer/>, document.getElementById('app') || document.body);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,29 @@
+var path = require('path');
+
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var path = require('path');
+const pkg = require('./package.json');
+
+const sassLoaders = [
+  'css-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, 'app/style')
+]
+
+const TARGET = process.env.npm_lifecycle_event;
 
 module.exports = {
   entry: {
-      'main': 'app/main'
+      'main': 'app/main',
+      'style': 'app/style/application.scss'
   },
   output: {
     filename: "[name].js",
-    path: "./build",
+    path: path.join(__dirname, './build'),
     sourceMapFilename: '[file].map'
   },
   devtool: 'source-map',
@@ -25,24 +39,28 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        exclude: /(node_modules|bower_components)/,
-        loaders: ['style', 'css', 'sass']
+        loader: "style!css!sass",
+        exclude: /(node_modules|bower_components)/ },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, loader: "file"
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'node_modules/html-webpack-template/index.ejs',
+      template: 'app/templates/index.ejs',
       title: 'crlsn app',
-      appMountId: 'app',
+      appMountId: ['app'],
       inject: false
-    })
+    }),
+    new ExtractTextPlugin('[name].css')
   ],
   resolve: {
     extensions: ['', '.js', '.jsx', '.scss'],
     modulesDirectories: ['node_modules'],
     root: [
-        path.resolve('./')
+        path.resolve('./'),
+        path.resolve('./app/style')
     ]
   }
 };
