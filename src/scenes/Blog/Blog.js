89d6af9a7f5d4aa9a96ad7c52a3963127bccca19
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-09-02 06:34:27 -0600

    Pass list of article IDs to Blog scene

--- src/scenes/Blog/Blog.js
+++ src/scenes/Blog/Blog.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Article from 'components/Article'
 
+const BlogPosts = [0, 1, 2]
+
+const articles = BlogPosts.map((id) =>
+  <Article id={id} />
+)
+
 const Blog = () => (
   <div>
-    <Article />
+    {articles}
   </div>
 )
 
