0c29824cd1ccbe5a60e0ed5ed4079f063b00ca53
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-09-02 10:44:43 -0600

    Add graph.cool endpoint

--- src/services/apollo/apollo.js
+++ src/services/apollo/apollo.js
@@ -3,7 +3,7 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo'
 const apollo = new ApolloClient({
   connectToDevTools: typeof window !== 'undefined',
   networkInterface: createNetworkInterface({
-    uri: 'http://api/graphql',
+    uri: 'https://api.graph.cool/simple/v1/cj73it8i001gu01144hccdwqo',
   }),
   queryDeduplication: true,
   shouldBatch: true,
