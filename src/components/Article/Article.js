fcabc308501930a5cda0d05ff3b9276adc886a78
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-09-02 08:54:05 -0600

    Convert Article to functional component

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,27 +1,25 @@
 import marked from 'marked'
-import React, { Component } from 'react'
+import React from 'react'
 
 import Tag from 'components/Tag'
 
-class Article extends Component {
-  displayName = 'Article'
-
-  getMarkdownText = () => {
-    var rawMarkup = marked('This is _Markdown_.', { sanitize: true })
-    return { __html: rawMarkup }
-  }
-
-  render = () => (
-    <section className="article">
-      <div>
-        <article>
-          <h1>{ 'Article Title' }</h1>
-          <div dangerouslySetInnerHTML={this.getMarkdownText()} />
-        </article>
-      </div>
-      <Tag />
-    </section>
-  )
+const getMarkdownText = () => {
+  var rawMarkup = marked('This is _Markdown_.', { sanitize: true })
+  return { __html: rawMarkup }
 }
+
+const Article = () => (
+  <section className="article">
+    <div>
+      <article>
+        <h1>{ 'Article Title' }</h1>
+        <div dangerouslySetInnerHTML={getMarkdownText()} />
+      </article>
+    </div>
+    <Tag />
+  </section>
+ )
+
+Article.displayName = 'Article'
 
 export default Article
