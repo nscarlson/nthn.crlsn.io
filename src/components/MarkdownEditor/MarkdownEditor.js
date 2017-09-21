31f3a22e84261eed29b3cac31938fb40c0e55479
Nathan Carlson <nathan.s.carlson@gmail.com>
2017-09-21 00:27:52 -0600

    Add MarkdownEditor css and formatting

--- src/components/MarkdownEditor/MarkdownEditor.js
+++ src/components/MarkdownEditor/MarkdownEditor.js
@@ -42,7 +42,9 @@ the inspiration to this, and some handy implementation hints, came.
 const MarkdownEditor = () => (
   <div>
     <div id="in">
-      <form><textarea defaultValue={text} id="code" /></form></div>
+      <form>
+        <textarea defaultValue={text} id="code" />
+      </form></div>
     <div id="out" />
   </div>
 )
