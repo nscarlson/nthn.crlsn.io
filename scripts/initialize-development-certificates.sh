e314ff5754f3b46afaedb3ec2e48099447a40a08
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-01-26 14:06:56 -0700

    configure nginx-proxy

--- scripts/initialize-development-certificates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CERTS_DIRECTORY=$(dirname $0)/../certs
-SCRIPTS_DIRECTORY=$(dirname $0)
-
-if [ ! -f $CERTS_DIRECTORY/certificate-authority.pem ]; then
-    $SCRIPTS_DIRECTORY/generate-development-certificates.sh
-fi
