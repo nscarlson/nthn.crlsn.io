4eb4664ef925af12bfee3cf9a15f5c2d9fbde08e
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-01-26 22:28:55 -0700

    temporarily disable generateBuildId

--- projects/web/next.config.js
+++ projects/web/next.config.js
@@ -1,10 +1,10 @@
 // next.config.js
 module.exports = {
     distDir: 'build',
-    generateBuildId: async () => {
-        return require('child_process')
-            .execSync('git rev-parse HEAD')
-            .toString()
-            .trim()
-    },
+    // generateBuildId: async () => {
+    //     return require('child_process')
+    //         .execSync('git rev-parse HEAD')
+    //         .toString()
+    //         .trim()
+    // },
 }
