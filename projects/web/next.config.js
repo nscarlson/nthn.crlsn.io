e7e5206e98d8d2829a9148098a831473b353e5b3
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-01-26 22:26:14 -0700

    configure now

--- projects/web/next.config.js
+++ projects/web/next.config.js
@@ -1,4 +1,10 @@
 // next.config.js
 module.exports = {
     distDir: 'build',
+    generateBuildId: async () => {
+        return require('child_process')
+            .execSync('git rev-parse HEAD')
+            .toString()
+            .trim()
+    },
 }
