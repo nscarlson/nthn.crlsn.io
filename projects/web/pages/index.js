54977a208a525ba5c60b24dfe97723dd7df2dbff
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-01-27 21:19:44 -0700

    remove sequelize, use square avatar

--- projects/web/pages/index.js
+++ projects/web/pages/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+const StyledAvatar = styled.div`
+    img {
+        border-radius:50%;
+
+        border-width: 3px;
+        border-style: solid;
+        border-image: linear-gradient(black, rgba(0, 0, 0, 0)) 1 100%;
+    }
+`
+
 const Index = () => (
-    <div>
-        <img src="/static/images/profile.jpg" />
-    </div>
+    <StyledAvatar>
+        <img src="/static/images/avatar.jpg" />
+    </StyledAvatar>
 )
 
 export default Index
