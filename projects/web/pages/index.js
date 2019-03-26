c5a162d944e38beb55cad0c26d79f4eae27d0481
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-03-26 00:18:50 -0600

    use flex-box

--- projects/web/pages/index.js
+++ projects/web/pages/index.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react'
 import styled from '@emotion/styled'
 
-const StyledAvatar = styled.img`
-    border-radius: 50%;
-    border-width: 0px;
-    height: auto;
-    width: 324px;
-    position: absolute;
-`
-
-const StyledContent = styled.div`
+const Centered = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
     height: 100vh;
 `
 
+const StyledAvatar = styled.img`
+    border-radius: 50%;
+`
+
+const AlignedContent = styled.div``
+
 const Index = () => (
     <div className="app">
-        <StyledContent>
+        <Centered>
             <StyledAvatar src="/static/images/avatar.jpg" />
-        </StyledContent>
+        </Centered>
     </div>
 )
 
