ad638626a747e37236ea382ebccd2846207c2081
Nathan Carlson <nathan.s.carlson@gmail.com>
2020-04-28 17:14:53 -0600

    deps: remove unneeded deps

--- projects/web/lib/apollo-client.tsx
+++ projects/web/lib/apollo-client.tsx
@@ -1,7 +1,5 @@
 import ApolloClient from 'apollo-client'
-import { ApolloLink } from 'apollo-link'
 import { createUploadLink } from 'apollo-upload-client'
-import { HttpLink } from 'apollo-link-http'
 import withApollo from 'next-with-apollo'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
@@ -16,14 +14,8 @@ export default withApollo(
         new ApolloClient({
             cache: new InMemoryCache().restore(initialState || {}),
             connectToDevTools: process.browser,
-            link: ApolloLink.from([
-                new HttpLink({
-                    fetch,
-                    uri: publicRuntimeConfig.GRAPHQL_ENDPOINT, // Server URL (must be absolute)
-                }),
-                createUploadLink({
-                    uri: publicRuntimeConfig.GRAPHQL_ENDPOINT,
-                }),
-            ]),
+            link: createUploadLink({
+                uri: publicRuntimeConfig.GRAPHQL_ENDPOINT,
+            }),
         }),
 )
