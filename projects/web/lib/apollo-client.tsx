2c08c1d81f27c7f5e05e3fbd6ad1677d6864e578
Nathan Carlson <nathan.s.carlson@gmail.com>
2020-05-16 21:33:41 -0600

    feat: compose httpLink and uploadLink

--- projects/web/lib/apollo-client.tsx
+++ projects/web/lib/apollo-client.tsx
@@ -1,12 +1,23 @@
 import ApolloClient from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-// import { createUploadLink } from 'apollo-upload-client'
+import { createUploadLink } from 'apollo-upload-client'
 import withApollo from 'next-with-apollo'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
+
+const httpLink = new HttpLink({
+    credentials: 'include',
+    fetch,
+    uri: publicRuntimeConfig.GRAPHQL_ENDPOINT, // Server URL (must be absolute)
+})
+
+const uploadLink = createUploadLink({
+    uri: publicRuntimeConfig.GRAPHQL_ENDPOINT,
+})
 
 // Export a HOC from next-with-apollo
 // Docs: https://www.npmjs.com/package/next-with-apollo
@@ -15,10 +26,6 @@ export default withApollo(
         new ApolloClient({
             cache: new InMemoryCache().restore(initialState || {}),
             connectToDevTools: process.browser,
-            link: new HttpLink({
-                credentials: 'include',
-                fetch,
-                uri: publicRuntimeConfig.GRAPHQL_ENDPOINT, // Server URL (must be absolute)
-            }),
+            link: ApolloLink.from([httpLink, uploadLink]),
         }),
 )
